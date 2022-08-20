aecee9438547782ae746d8c9cbc8f17b7d7797af
Ben Kiboma <omayioben@gmail.com>
2022-08-20 14:26:54 +0300

    Conditional statements

---
+++ conditionals.rb
@@ -0,0 +1,5 @@
+if 1 > 3
+	puts "1 is less than 3"
+else
+  puts "1 is more than 3"
+end
---
+++ conditionals2.rb
@@ -0,0 +1,10 @@
+is_student = true
+is_smart = false
+
+if is_student and is_smart
+	puts "you are a smart student"
+elsif is_student and !is_smart
+	puts "You are not a smart student"
+else
+	puts "Not a student and not smart"
+end
